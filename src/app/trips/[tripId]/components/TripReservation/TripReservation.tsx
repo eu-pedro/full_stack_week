65f216980dea42a530a0ffbbaca453f117292c1e
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-07-15 11:00:24 -0300

    feat: add interface to trip confirmation

--- src/app/trips/[tripId]/components/TripReservation/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation/TripReservation.tsx
@@ -3,6 +3,7 @@ import Button from "@/components/Button/Button";
 import DatePicker from "@/components/DatePicker/DatePicker";
 import Input from "@/components/Input/Input";
 import { differenceInDays } from "date-fns";
+import { useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
 
 interface TripReservationProps {
@@ -29,6 +30,8 @@ export function TripReservation({ maxGuests, tripStartDate, tripEndDate, pricePe
     watch, 
     setError 
   } = useForm<TripReservationForm>()
+
+  const router = useRouter()
   
   const onSubmit = async (data: TripReservationForm) => {
     const response = await fetch('http://localhost:3000/api/trips/check', {
@@ -56,7 +59,7 @@ export function TripReservation({ maxGuests, tripStartDate, tripEndDate, pricePe
     }
 
     if(res?.error?.code === 'INVALID_START_DATE'){
-      setError('startDate', {
+      return setError('startDate', {
         type: 'manual',
         message: 'Data inválida'
       })
@@ -68,7 +71,11 @@ export function TripReservation({ maxGuests, tripStartDate, tripEndDate, pricePe
         message: 'Data inválida'
       })
     }
+
+    router.push(`/trips/${tripId}/confirmation?startDate=${data.startDate?.toISOString()}&endDate=${data.endDate?.toISOString()}&guests=${data.guests}`)
   }
+
+
 
 
   const startDate = watch('startDate')
---
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -0,0 +1,88 @@
+'use client'
+import { Trip } from "@prisma/client";
+import { format } from "date-fns";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import ReactCountryFlag from "react-country-flag";
+import ptBR from "date-fns/locale/pt-BR";
+import Button from "@/components/Button/Button";
+
+
+export default function TripConfirmation({ params }: { params: { tripId: string}}) {
+  const [trip, setTrip] = useState<Trip | null>()  
+  const [totalPrice, setTotalPrice] = useState<number>(0)
+  
+  const searchParams = useSearchParams()
+
+
+  useEffect(() => {
+    const fetchTrip = async () => {
+      const response = await fetch(`/api/trips/check`, {
+        method: "POST",
+        body: JSON.stringify({
+          tripId: params.tripId,
+          startDate: searchParams.get("startDate"),
+          endDate: searchParams.get("endDate"),
+        }),
+      });
+
+      const { trip, totalPrice } = await response.json();
+      setTotalPrice(totalPrice)
+      setTrip(trip)
+    };
+
+    fetchTrip()
+  }, [])
+
+  const startDate = new Date(searchParams.get('startDate') as string)
+  const endDate = new Date(searchParams.get('endDate') as string)
+  const guests = searchParams.get('guests')
+
+  if(!trip) return null
+
+  return (
+    <div className="container mx-auto p-5 h-screen">
+      <h1 className="text-xl font-semibold text-primaryDarker">Sua viagem</h1>
+      {/* CARD */}
+      <div className="flex flex-col p-5 mt-5 border-solid border border-grayLighter rounded-lg shadow-md">
+        <div className="flex items-center gap-3 border-b border-grayLighter border-solid pb-5">
+          <div className="relative w-[124px] h-[106px]">
+            <Image src={trip?.coverImage} alt={trip.name} fill style={{backgroundImage: 'cover'}} className="rounded-md"/>
+          </div>
+          <div className="flex flex-col">
+            <h2 className="text-xl text-primaryDarker font-semibold">{trip.name}</h2>
+            <div className="flex items-center gap-1">
+              <ReactCountryFlag countryCode={trip.countryCode} svg />
+              <p className="text-xs text-grayPrimary">{trip.location}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-3">
+          <h3 className="text-sm text-primaryDarker font-semibold mt-5">Informações do preço</h3>
+
+          <div className="flex justify-between items-center">
+            <p className="text-primaryDarker text-sm font-normal">Total: </p>
+            <p className="font-medium text-primaryDarker ">R${totalPrice}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col mt-5">
+          <h3 className="text-primaryDarker font-semibold  text-sm">Data</h3>
+          <div className="flex items-center gap-1">
+            <p className="text-primaryDarker font-normal text-sm">{format(startDate, "dd 'de' MMMM ", { locale: ptBR })}</p>
+            {' - '}
+            <p className="text-primaryDarker font-normal text-sm">{format(endDate, "dd 'de' MMMM ", { locale: ptBR })}</p>
+          </div>
+
+          <h3 className="text-primaryDarker font-semibold mt-5">Hóspedes</h3>
+          <p className="text-primaryDarker font-normal text-sm">{guests} hóspedes</p>
+
+          <Button className="mt-5">Finalizar Compra</Button>
+      </div>
+
+    </div>
+  )
+}
