62559c629cb8cc07eb015166569f53c2143cfc95
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-07-20 20:35:06 -0300

    feat: add user reservation pages

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -2,21 +2,25 @@
 import { useSession } from "next-auth/react"
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
-import { TripReservation } from "@prisma/client"
+import { Prisma, TripReservation } from "@prisma/client"
+import { UserReservationItem } from "./components/UserReservationItem"
 
-export default function MyTrips () {
-  const [reservations, setReservations] = useState<TripReservation[]>([])
+
+export default function MyTrips() {
+  const [reservations, setReservations] = useState<Prisma.TripReservationGetPayload<{
+    include: { trip: true }
+  }>[]>([])
   const { status, data } = useSession()
 
 
   const router = useRouter()
 
   useEffect(() => {
-     if(status === 'unauthenticated') {
-       router.push('/')
-     }
+    if (status === 'unauthenticated') {
+      router.push('/')
+    }
 
-    const fetchReservations = async ()  => {
+    const fetchReservations = async () => {
       const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any)?.id}/reservations`)
 
       const json = await response.json()
@@ -24,9 +28,16 @@ export default function MyTrips () {
     }
     fetchReservations()
   }, [status])
-  
-  console.log({reservations})
+
+  console.log({ reservations })
 
 
-  return <h1>my trips </h1>
+  return (
+    <div className="contaner mx-auto p-5">
+      <h1 className="font-semibold text-primaryDarker text-xl">Minhas Viagens</h1>
+      {reservations?.map((reservation) => (
+        <UserReservationItem reservation={reservation} key={reservation.id} />
+      ))}
+    </div>
+  )
 }
