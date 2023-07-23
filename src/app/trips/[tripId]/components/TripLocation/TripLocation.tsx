9f6f5beb58df5dc9d546392df85572c53f2259b6
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-07-23 14:13:28 -0300

    style: adapt trip details to desktop

--- src/app/trips/[tripId]/components/TripLocation/TripLocation.tsx
+++ src/app/trips/[tripId]/components/TripLocation/TripLocation.tsx
@@ -11,9 +11,9 @@ export function TripLocation({
   locationDescription,
 }: TripLocationProps) {
   return (
-    <div className="p-5">
-      <h2 className="font-semibold text-primaryDarker mb-5">Destaques</h2>
-      <div className="relative h-[280px] w-full mb-5">
+    <div className="p-5 lg:p-0 lg:pb-20">
+      <h2 className="font-semibold text-primaryDarker mb-5 lg:text-xl">Destaques</h2>
+      <div className="relative h-[280px] w-full mb-5 lg:hidden">
         <Image
           src="/map-mobile.png"
           alt=""
@@ -24,9 +24,20 @@ export function TripLocation({
           className="rounded-lg shadow-md"
         />
       </div>
-      <p className="primaryDarker text-sm font-semibold mt-3">{location}</p>
+      <div className="relative hidden lg:block h-[480px] w-full mb-5">
+        <Image
+          src="/map-desktop.png"
+          alt=""
+          fill
+          style={{
+            objectFit: 'cover',
+          }}
+          className="rounded-lg shadow-md"
+        />
+      </div>
+      <p className="primaryDarker text-sm font-semibold mt-3 lg:text-base lg:mt-5">{location}</p>
 
-      <p className="text-xs text-primaryDarker mt-3 leading-5">
+      <p className="text-xs text-primaryDarker mt-3 leading-5 lg:text-base lg:mt-4">
         {locationDescription}
       </p>
       <Button variant="outlined" className="w-full mt-5">
